525e401d0fcd471e1ae74a32bbe5d7f30e1c4ec4
aarnaTechx <128452452+aarnaTechx@users.noreply.github.com>
2024-10-06 09:34:50 +0530

    6 OCT Demo

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -69,6 +69,7 @@ const Home = () => {
                 key={profile.id}
                 name={profile.firstName} // Change from firstName to name
                 organization={profile.clubName} // Ensure this matches your Profile interface
+                image={profile.image}
                 rating={profile.rating}
                 slug={profile.slug} // Ensure slug is also part of Profile interface
               />
